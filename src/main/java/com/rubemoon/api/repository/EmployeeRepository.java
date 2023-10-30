79290d72c61f165a8a9a84b70b1f4f202f06f21e
rubemoon <aprovadosatisfy@gmail.com>
2023-10-29 22:36:45 -0300

    Adds the mvc

---
+++ src/main/java/com/rubemoon/api/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.rubemoon.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.rubemoon.api.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+
+}
---
+++ src/main/java/com/rubemoon/api/service/EmployeeService.java
@@ -0,0 +1,36 @@
+package com.rubemoon.api.service;
+
+import com.rubemoon.api.model.Employee;
+import com.rubemoon.api.repository.EmployeeRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Data
+@Service
+public class EmployeeService {
+
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Optional<Employee> getEmployee(final Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Iterable<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void deleteEmployee(final Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        Employee savedEmployee = employeeRepository.save(employee);
+        return savedEmployee;
+    }
+}
+
